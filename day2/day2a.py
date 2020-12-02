71956493ae57cf5c822b4530cd7c5d7bd53125bc
Frédéric Seraphine <seraphin@mbp13fse.home>
2020-12-02 22:06:46 +0100

    Initial commit

---
+++ day2/day2a.py
@@ -0,0 +1,15 @@
+import re
+
+pattern = re.compile('(\d+)-(\d+) (\w): (\w+)\n')
+
+nb=0
+
+f = open('day2_input.txt')
+for line in f:
+    min,max,letter,password = pattern.match(line).groups()
+    print(min,max,letter,password)
+    password  = ''.join(sorted(password))
+    if (letter*int(min) in password) and (letter*(int(max)+1) not in password):
+        nb = nb +1
+print nb
+
---
+++ day2/day2b.py
@@ -0,0 +1,14 @@
+import re
+
+pattern = re.compile('(\d+)-(\d+) (\w): (\w+)\n')
+
+nb=0
+
+f = open('day2_input.txt')
+for line in f:
+    p1,p2,letter,password = pattern.match(line).groups()
+    print(p1,p2,letter,password)
+    if (password[int(p1)-1] == letter) ^ (password[int(p2)-1] == letter):
+        nb = nb +1
+print nb
+
