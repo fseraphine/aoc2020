e7989a8231bc21077c31ac7592d35d1fe1aef08d
Frédéric Seraphine <seraphin@mbp13fse.home>
2020-12-02 22:10:41 +0100

    remove traces

--- day2/day2a.py
+++ day2/day2a.py
@@ -7,7 +7,6 @@ nb=0
 f = open('day2_input.txt')
 for line in f:
     min,max,letter,password = pattern.match(line).groups()
-    print(min,max,letter,password)
     password  = ''.join(sorted(password))
     if (letter*int(min) in password) and (letter*(int(max)+1) not in password):
         nb = nb +1
